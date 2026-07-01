0d5c7c1ea3f3526949af17e55e4f0c1ffcc3a832
unknown <joshstat126@gmail.com>
2026-07-01 19:34:01 +0800

    Rename `model_type = x_by_block` to `x_by_b`

--- R/friedman-test-defs.R
+++ R/friedman-test-defs.R
@@ -1,5 +1,5 @@
 friedman_def_xby = statim::stat_define(
-    model_type = x_by_block,
+    model_type = x_by_b,
     impl = statim::agendas(
         base = statim::baseline(fn = function(.proc) {
             friedman_test_group(
