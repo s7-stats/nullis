d1fb3287075f1f3b509af5c4584834b99c8624d4
unknown <joshstat126@gmail.com>
2026-07-01 20:24:18 +0800

    New `base` output; temporary `print` characterization

--- R/kruskal-test-defs.R
+++ R/kruskal-test-defs.R
@@ -1,8 +1,20 @@
 kwtest_def = statim::stat_define(
     model_type = x_by,
     impl = statim::agendas(
-        base = statim::baseline(fn = function(.proc) {
-            kruskal_wallis_group(.proc$x_data[[1]], .proc$group_data[[1]])
-        })
+        base = statim::baseline(
+            fn = function(.proc) {
+                out = kruskal_wallis_group(.proc$x_data[[1]], .proc$group_data[[1]])
+
+                class_kw_test(
+                    # vars = names(.proc$group_data),
+                    statistic = out$statistic,
+                    df = out$df,
+                    p_value = out$p_value
+                )
+            },
+            print = function(x, ...) {
+                print(x@data@p_value)
+            }
+        )
     )
 )
