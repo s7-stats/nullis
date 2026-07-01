3a2fd6542911dcfe6e9a91aea4b108e528d92cf0
unknown <joshstat126@gmail.com>
2026-07-01 20:10:04 +0800

    Add `class_kw_test` S7 class

--- R/kruskal-test.R
+++ R/kruskal-test.R
@@ -6,3 +6,22 @@ KW_TEST = statim::HTEST_FN(
     defs = list(kwtest_def),
     "Kruskal-Wallis Test"
 )
+
+#' @export
+class_kw_test = S7::new_class(
+    "kw_test",
+    parent = statim::class_stat_infer,
+    properties = list(
+        vars = S7::class_character,
+        statistic = S7::class_numeric,
+        df = S7::class_numeric,
+        p_value = S7::new_property(
+            class = S7::class_numeric,
+            validator = function(self) {
+                if (!(self > 0 && self < 1)) {
+                    "p value must be between 0 and 1 only. "
+                }
+            }
+        )
+    )
+)
