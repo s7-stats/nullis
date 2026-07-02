1b0224db65692e446fbff5078aa36f38bf9425ca
unknown <joshstat126@gmail.com>
2026-07-02 13:51:01 +0800

    Add `pval_styler`, instead of repeating the same code

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,13 @@
 }
 
 `%notin%` = Negate(`%in%`)
+pval_styler = function(x) {
+    x_num = suppressWarnings(as.numeric(x$value))
+    if (is.na(x_num) || x_num > 0.05) {
+        cli::style_italic(x$value)
+    } else if (x_num > 0.01) {
+        cli::col_red(x$value)
+    } else {
+        cli::style_bold("<0.001")
+    }
+}
