d8f957456c6e3bf9adf1a0035aad5638ec54b4a2
unknown <joshstat126@gmail.com>
2026-07-02 13:52:04 +0800

    New `kwtest_def_on` for Kruskal-Wallis `on()` pipeline

--- R/kruskal-test-defs-on.R
+++ R/kruskal-test-defs-on.R
@@ -1,3 +1,25 @@
-# kwtest_def_on = statim::stat_define(
-#     model_type = on
-# )
+kwtest_def_on = statim::stat_define(
+    model_type = on,
+    impl = statim::agendas(
+        base = statim::baseline(
+            fn = function(.proc) {
+                kruskal_wallis_cpp(.proc$data)
+            },
+            print = function(x, ...) {
+                out = x@data
+
+                df_res = tibble::tibble(
+                    `H Statistic` = out$statistic,
+                    `Degrees of Freedom` = out$df,
+                    `p-value` = out$p_value
+                )
+
+                tabstats::table_default(
+                    df_res,
+                    style_columns = tabstats::td_style("p value" = pval_styler)
+                )
+                cat("\n")
+            }
+        )
+    )
+)
