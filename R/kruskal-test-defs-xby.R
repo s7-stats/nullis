08d3e4335fac63d803110e66bcda23d58d9c4970
unknown <joshstat126@gmail.com>
2026-07-02 14:07:45 +0800

    Complete `pairwise` method (might write C++ version soon)

--- R/kruskal-test-defs-xby.R
+++ R/kruskal-test-defs-xby.R
@@ -16,15 +16,55 @@ kwtest_def_xby = statim::stat_define(
             }
         ),
         pairwise = statim::variant(
-            fn = function(.proc) {
+            fn = function(.proc, .ci = 0.95) {
                 # curr_data = imap(.proc$data, \(x, i) tibble(group = i, value = x))
                 curr_data = .proc$x_data[[1]]
                 group_data = .proc$group_data[[1]]
 
                 r = rank(curr_data)
-                statistic = kruskal_wallis_group(
-                    curr_data,
-                    group_data
+                kw = kruskal_wallis_group(curr_data, group_data)
+                h_stat = kw$statistic
+
+                groups = unique(group_data)
+                k = length(groups)
+                n = tapply(curr_data, group_data, length)
+                N = length(curr_data)
+                mean_r = tapply(r, group_data, mean)
+
+                df_resid = N - k
+                s2 = N * (N + 1) / 12
+                pooled_var = s2 * (N - 1 - h_stat) / df_resid
+
+                pairs = combn(groups, 2, simplify = FALSE)
+                group_a = purrr::map_chr(pairs, 1)
+                group_b = purrr::map_chr(pairs, 2)
+
+                comparison = paste(group_a, "and", group_b)
+
+                diff = purrr::map2_dbl(
+                    group_a,
+                    group_b,
+                    ~ mean_r[[.x]] - mean_r[[.y]]
+                )
+
+                std_err = purrr::map2_dbl(
+                    group_a,
+                    group_b,
+                    ~ sqrt(pooled_var * (1 / n[[.x]] + 1 / n[[.y]]))
+                )
+
+                test_stat = diff / std_err
+                p_value = 2 * pt(abs(test_stat), df_resid, lower.tail = FALSE)
+                p_adj = p.adjust(p_value, method = "holm")
+
+                tibble::tibble(
+                    comparison = comparison,
+                    diff = diff,
+                    std_err = std_err,
+                    statistic = test_stat,
+                    df = df_resid,
+                    p_value = p_value,
+                    p_adj = p_adj
                 )
             }
         )
