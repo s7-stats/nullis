230a755f0ff2e19557bbe85eda246258a8ba251b
unknown <joshstat126@gmail.com>
2026-07-02 13:35:14 +0800

    Rename to `kruskal-test-defs-xby.R` instead

--- R/kruskal-test-defs-xby.R
+++ R/kruskal-test-defs-xby.R
@@ -14,18 +14,11 @@ kwtest_def = statim::stat_define(
                     p_value = vapply(tests, \(t) t$p_value, numeric(1))
                 )
             }
+        ),
+        pairwise = statim::variant(
+            fn = function(.proc) {
+                curr_data = imap(.proc$data, \(x, i) tibble(group = i, value = x))
+            }
         )
-        # multi = statim::variant(fn = function(.proc) {
-        #     tests = lapply(.proc$group_data, function(g) {
-        #         kruskal_wallis_group(.proc$x_data[[1]], g)
-        #     })
-        #
-        #     class_kw_test(
-        #         vars = names(.proc$group_data),
-        #         statistic = vapply(tests, \(t) t$statistic, numeric(1)),
-        #         df = vapply(tests, \(t) t$df, numeric(1)),
-        #         p_value = vapply(tests, \(t) t$p_value, numeric(1))
-        #     )
-        # })
     )
 )
