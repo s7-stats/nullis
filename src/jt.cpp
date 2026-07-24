e08a94f1963e640b3c4fdcc14d78ade9c268149c
unknown <joshstat126@gmail.com>
2026-07-24 18:17:17 +0800

    Bug fix: Add `wilcox_free()` escape hatch

--- src/jt.cpp
+++ src/jt.cpp
@@ -121,6 +121,7 @@ std::vector<double> jt_pdf(const std::vector<int>& group_sizes) {
         mn_prev = mn + mn_prev;
     }
 
+    wilcox_free();
     return pdf;
 }
 
@@ -167,12 +168,6 @@ jtObject compute_jt(
         method = "exact";
         std::vector<double> pdf = jt_pdf(group_sizes);
         int jt_int = static_cast<int>(std::round(statistic));
-        double pdf_sum = std::accumulate(pdf.begin(), pdf.end(), 0.0);
-        Rcpp::Rcout << "jt_int=" << jt_int
-                    << " pdf_size=" << pdf.size()
-                    << " pdf_sum=" << pdf_sum
-                    << " statistic=" << statistic
-                    << "\n";
 
         // double p_lower = std::accumulate(pdf.begin(), pdf.begin() + jt_int, 0.0);
         double p_lower = std::accumulate(pdf.begin(), pdf.begin() + jt_int + 1, 0.0);
